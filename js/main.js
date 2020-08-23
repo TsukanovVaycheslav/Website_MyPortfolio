1a9578cea9ad771876a62ebea6324b13b898ddb5
TsukanovVaycheslav <58068725+TsukanovVaycheslav@users.noreply.github.com>
2020-08-23 22:26:19 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 new WOW().init();
 
+// Прокрутка "Последние проекты"
 var mySwiper = new Swiper('.swiper-container', {
     pagination: {
       el: '.projects-pagination',
@@ -9,6 +10,29 @@ var mySwiper = new Swiper('.swiper-container', {
       clickable: true,
     },
     autoplay: {
-      delay: 5000,
+      delay: 7000,
     }
   });
+
+// Меняет фон страницы
+const chk = document.getElementById('check1');
+
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
+
+
+
+// Прокрутка страницы
+$(document).ready(function () {
+
+	$('a[href^="#"]').click(function () {
+		//Сохраняем значение атрибута href в переменной:
+		var target = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top //можно вычесть высоту меню
+		}, 1100);
+		return false;
+	});
+
+});
